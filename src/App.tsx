9aa4aa4f57d6e43bb897f5396bd5f4fa1bc81375
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-06 17:55:10 -0300

    feat: :sparkles: Added components cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCart } from "./components/note-card";
 
 export function App() {
   return (
@@ -17,58 +19,13 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, et
-            incidunt? Sint sunt labore provident molestiae neque. Asperiores,
-            possimus reiciendis omnis doloremque dicta quasi eum quam tempore
-            veniam ipsum iste. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Aspernatur consequuntur nulla voluptatum cumque
-            ipsum rerum in esse eos numquam? Nesciunt eaque ut amet tempora
-            veritatis dolorem laboriosam quaerat facere consectetur?
-          </p>
-
-          <div className="absolute  bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3  overflow-hidden relative ">
-          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            consequuntur nulla voluptatum cumque ipsum rerum in esse eos
-            numquam? Nesciunt eaque ut amet tempora veritatis dolorem laboriosam
-            quaerat facere consectetur?
-          </p>
-
-          <div className="absolute  bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3  overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            consequuntur nulla voluptatum cumque ipsum rerum in esse eos
-            numquam? Nesciunt eaque ut amet tempora veritatis dolorem laboriosam
-            quaerat facere consectetur?
-          </p>
-
-          <div className="absolute  bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
+        <NewNoteCard />
+        <NoteCart />
+        <NoteCart />
+        <NoteCart />
+        <NoteCart />
+        <NoteCart />
+        <NoteCart />
       </div>
     </div>
   );
