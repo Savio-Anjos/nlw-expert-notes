4e75a8c9f477b1b2ca528b8d476b0b2fe5a661b2
savioanjos2020@gmail.com <savioanjos2020@gmail.com>
2024-02-12 10:38:02 -0300

    feat :sparkles: Added modifications dynamics

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,20 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { ChangeEvent, useState } from "react";
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnBoarding] = useState(true);
+
+  function handleStartEdditor() {
+    setShouldShowOnBoarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === "") {
+      setShouldShowOnBoarding(true);
+    }
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left outline-none p-5 gap-3  hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -26,17 +39,28 @@ export function NewNoteCard() {
               Adicionar nota
             </span>
 
-            <p className="text-sm leading-6 text-slate-400 ">
-              Comece{" "}
-              <button className="font-medium text-lime-400 hover:underline">
-                gravando uma nota
-              </button>{" "}
-              em áudio ou se preferir{" "}
-              <button className="font-medium text-lime-400 hover:underline">
-                utilize apenas texto
-              </button>
-              .
-            </p>
+            {shouldShowOnboarding ? (
+              <p className="text-sm leading-6 text-slate-400 ">
+                Comece{" "}
+                <button className="font-medium text-lime-400 hover:underline">
+                  gravando uma nota
+                </button>{" "}
+                em áudio ou se preferir{" "}
+                <button
+                  onClick={handleStartEdditor}
+                  className="font-medium text-lime-400 hover:underline"
+                >
+                  utilize apenas texto
+                </button>
+                .
+              </p>
+            ) : (
+              <textarea
+                autoFocus
+                className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChanged}
+              />
+            )}
           </div>
 
           <button
